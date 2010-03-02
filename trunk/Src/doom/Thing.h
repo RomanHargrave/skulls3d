23655d41305228c0ee2b0e186d71b60b510941cb
gabriel.bizzotto <gabriel.bizzotto@9012b7bc-1e1c-11df-ae22-3371deb0c1a1>
2010-03-02 01:45:48 +0000

    Definition of thing types.

--- trunk/Src/doom/Thing.h
+++ trunk/Src/doom/Thing.h
@@ -2,9 +2,12 @@
 #ifndef SK_THINGLUMP
 #define SK_THINGLUMP
 
+#include <vector>
+
 namespace doom
 {
 	class WadFile;
+	class PatchLump;
 
 	class Thing
 	{
@@ -12,17 +15,22 @@ namespace doom
 		short m_x;
 		short m_y;
 		short m_angle;
+		unsigned short m_type;
 		bool m_is_on_skill_1_2;
 		bool m_is_on_skill_3;
 		bool m_is_on_skill_4_5;
 		bool m_deaf;
 		bool m_not_in_single_player;
 
+		std::vector<PatchLump*> m_sprites;
+
 		Thing(WadFile * wadFile, int offset);
+		virtual ~Thing();
 
 		virtual int Load();
 		virtual void UnLoad();
 	};
 };
+
 
 #endif // SK_THINGLUMP
