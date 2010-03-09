e9eba6d37a147ca61de865442377778979307ae2
gabriel.bizzotto <gabriel.bizzotto@9012b7bc-1e1c-11df-ae22-3371deb0c1a1>
2010-03-09 00:04:49 +0000

    Shows top menu.

--- trunk/Src/display.h
+++ trunk/Src/display.h
@@ -8,7 +8,7 @@
 #include "SDL/SDL.h"
 #endif
 
-typedef struct { int x,y,w,h; }Rect;
+typedef struct { unsigned int x,y,w,h; }Rect;
 
 extern unsigned int g_scr_w;
 extern unsigned int g_scr_h;
@@ -24,14 +24,10 @@ void PutMapPixel(SDL_Surface *screen, int x, int y, int color);
 void PutPixel(SDL_Surface *screen, int x, int y, int color);
 void Put4Pixels(SDL_Surface *screen, int x, int y, int color);
 
-void Draw(SDL_Surface *screen,
-		  unsigned int * bitmap, Rect dimensions,
-		  Rect bitmap_what, Rect screen_where,
-		  unsigned char shade);
-
-void DrawBackGround(SDL_Surface *screen,
-					unsigned int * bitmap, unsigned int bitmap_w, unsigned int bitmap_h,
-					unsigned char shade);
+void Draw_320x200(SDL_Surface *screen,
+		          unsigned int * bitmap, Rect dimensions,
+				  Rect bitmap_what, Rect screen_where,
+				  unsigned char shade);
 
 void RefreshDebugDisplay(SDL_Surface *screen);
 
